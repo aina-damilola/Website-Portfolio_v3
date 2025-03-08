ed92747f3d4778f19ff6dbfda4045bcd739e7ca5
Damilola Aina <damilolaaina@Damilolas-MacBook-Air.local>
2025-03-08 04:40:26 -0500

    small updates

---
+++ Portfolio/src/apps/text_file.jsx
@@ -0,0 +1,13 @@
+import "./styles/text_file.css"
+
+import icon_tf from "../assets/text_file_icon.png"
+
+function Text_file(props){
+    return(
+        <div id="main_tf">
+
+        </div>
+    )
+}
+
+export default Text_file;
